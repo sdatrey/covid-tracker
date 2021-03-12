f31ea4b8edf4bd0d3898c6aa39e15a14fd9667a8
sdatrey <shouryadevatrey@gmail.com>
2021-03-13 00:54:40 +0530

    Header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CountriesComponent } from './components/countries/countries.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
