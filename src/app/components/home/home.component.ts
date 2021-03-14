e5f3d5739786227af9772a1e04532b8a90a0803a
sdatrey <shouryadevatrey@gmail.com>
2021-03-14 11:55:40 +0530

    Getting data from api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalDataSummary } from 'src/app/models/global-data';
+import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  globalData: GlobalDataSummary[];
+  constructor(private dataservice: DataServiceService) { }
 
   ngOnInit(): void {
+    this.dataservice.getGlobalData().subscribe({
+      next: (data) => {
+        console.log(data)
+        this.globalData = data;
+        data.forEach(cs => {
+          if(!Number.isNaN(cs.confirmed)){
+          this.totalConfirmed+= cs.confirmed;
+          this.totalActive+= cs.active;
+          this.totalDeaths+= cs.deaths;
+          this.totalRecovered+= cs.recovered; 
+          }
+
+        })
+      }
+    })
   }
 
 }
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,15 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { GlobalDataSummary } from '../models/global-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-10-2021.csv`;
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-12-2021.csv`;
 
   constructor(private http: HttpClient) { }
   getGlobalData(){
-
+    return this.http.get(this.globalDataUrl, {responseType: 'text'}).pipe(
+      map(response => {
+        let rows = response.split("\n");
+        let raw = {}
+        rows.splice(0,1);
+        let data: GlobalDataSummary[] = [];
+        rows.forEach(
+          row => {
+            let cols = row.split(/,(?=\S)/);
+            let cs = {
+              country: cols[3],
+              confirmed: +cols[7],
+              deaths: +cols[8],
+              active: +cols[10],
+              recovered: +cols[9] 
+            }
+            let temp: GlobalDataSummary = raw[cs.country];
+            if(temp){
+              temp.active = cs.active + temp.active;
+              temp.recovered = cs.recovered + temp.recovered;
+              temp.deaths = cs.deaths + temp.deaths;
+              raw[cs.country] = temp;
+            }
+            else{
+              raw[cs.country] = cs;
+            }
+          }
+        )
+        return <GlobalDataSummary[]>Object.values(raw);
+      }
+      )
+    )
   }  
 }
